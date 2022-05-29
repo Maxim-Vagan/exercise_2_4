4f772fb5bcb5a53d35daf921aab8153e691eef84
Maxim_Vagan <maxut@gmail.com>
2022-05-29 19:00:35 +0300

    Максим Сергеевич: ПР 2

--- src/main/java/ru/maxvagan/exercise24/CalculatorController.java
+++ src/main/java/ru/maxvagan/exercise24/CalculatorController.java
@@ -16,22 +16,18 @@ public class CalculatorController {
     public String index() {
         return RespServ.getMain();
     }
-
     @GetMapping(path = "/plus")
     public String postPlusArithmetic(@RequestParam("num1") String num1, @RequestParam("num2") String num2) {
         return RespServ.postPlusArithmetic(num1, num2);
     }
-
     @GetMapping(path = "/minus")
     public String postMinusArithmetic(@RequestParam("num1") String num1, @RequestParam("num2") String num2) {
         return RespServ.postMinusArithmetic(num1, num2);
     }
-
     @GetMapping(path = "/multiply")
     public String postMultiplyArithmetic(@RequestParam("num1") String num1, @RequestParam("num2") String num2) {
         return RespServ.postMultiplyArithmetic(num1, num2);
     }
-
     @GetMapping(path = "/divide")
     public String postDivideArithmetic(@RequestParam("num1") String num1, @RequestParam("num2") String num2) {
         return RespServ.postDivideArithmetic(num1, num2);
--- src/main/java/ru/maxvagan/exercise24/CalculatorService.java
+++ src/main/java/ru/maxvagan/exercise24/CalculatorService.java
@@ -2,7 +2,6 @@ package ru.maxvagan.exercise24;
 
 public interface CalculatorService {
     String getMain();
-
     String postPlusArithmetic(String num1, String num2);
     String postMinusArithmetic(String num1, String num2);
     String postMultiplyArithmetic(String num1, String num2);
